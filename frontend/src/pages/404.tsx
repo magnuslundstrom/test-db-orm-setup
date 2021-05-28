2caa128b9498ba26d4f8c5c28dec0bad0217a7b6
magnuslundstrom <45564667+magnuslundstrom@users.noreply.github.com>
2021-05-28 17:46:04 +0200

    404

---
+++ frontend/src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { Layout } from '@components/global/layout/Layout';
+export default function Custom404() {
+  return (
+    <Layout title="404 - Page was not found">
+      <h1>Page was not found.</h1>
+    </Layout>
+  );
+}
